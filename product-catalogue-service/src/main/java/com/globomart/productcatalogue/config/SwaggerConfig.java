09179763351b1084e60e5c1ae99826835411b5ee
Iswar <iswar.java9@gmail.com>
2018-07-26 08:50:48 +0530

    Multiple URI paths configuration for Swagger Documentation

--- product-catalogue-service/src/main/java/com/globomart/productcatalogue/config/SwaggerConfig.java
+++ product-catalogue-service/src/main/java/com/globomart/productcatalogue/config/SwaggerConfig.java
@@ -2,16 +2,21 @@ package com.globomart.productcatalogue.config;
 
 
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import com.google.common.base.Predicates;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @Configuration
 @EnableSwagger2
@@ -33,8 +38,10 @@ public class SwaggerConfig extends WebMvcConfigurationSupport{
 	    public Docket api() {  
 	        return new Docket(DocumentationType.SWAGGER_2)   
 	          .select()                                   
-	          .apis(RequestHandlerSelectors.any())               
-	          .paths(PathSelectors.any())                           
+	          .apis(RequestHandlerSelectors.basePackage("com.globomart.productcatalogue.controller"))               
+	          .paths(Predicates.or(regex("/v2/api/products.*"),regex("/api/products.*"))) // for different URI paths
+	          //.paths(regex("/v2/api/products.*"))
+	          
 	          .build();                                            
 	    }
 	  
